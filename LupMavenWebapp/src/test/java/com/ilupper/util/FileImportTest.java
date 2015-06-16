2e899022fdbf02f0db3374c5e555ffe2e0260b86
ilupper <lupma@yahoo.com>
2015-06-16 15:13:12 -0400

    Update FileImportTest.java
    sketching up possibilities in operations of files - testing

--- LupMavenWebapp/src/test/java/com/ilupper/util/FileImportTest.java
+++ LupMavenWebapp/src/test/java/com/ilupper/util/FileImportTest.java
@@ -1,5 +1,65 @@
 package com.ilupper.util;
 
+
 public class FileImportTest {
 
-}
+    //successful read by full size
+    /*@Test
+    public void testRead() {
+        
+    }*/
+    
+    //successful read by line
+    /*@Test
+    public void testReadByLine() {
+        
+    }*/
+    
+    //specific: unsuccessful read by full size
+    
+    //specific: unsuccessful read by line
+    
+    //both: file not found
+    
+    //both: file not readable/interrupted
+    
+    //both: content failures
+    
+        //empty line
+    
+        //empty space test (look below)
+    
+        //csv specific
+            //bad # of tokens in 1 line
+            
+            //incorrect designation of delimiter
+            
+    
+    //specific: ready by line content failure 
+        //carriage return
+    
+        //new line
+    
+        //empty last line
+    
+        //empty beginning line
+    
+    //TODO: generalized content test for trailing spaces
+
+    //TODO: generalized break from grid structure (from empty line)
+    //TODO: generalized break from grid structure (by missing delimiter)
+        //a) if over token
+    
+        //b) if under token
+    
+        //c) both under & over token: break from pattern of data
+        //*this is more of a data remediation task (in effort to save some data in case multiple rows
+            //exhibit malform qualities (more of a structural concept)
+    
+            //i) type (qa) - pre-advance knowledge of schema
+            //ii) length (qty)
+            //iii) by pattern (like combo of numeric and string)
+            //iv) regex pattern?
+            //v) by plurality of pattern (majority)
+    //TODO: generalized break from grid structure should be just general failure
+}   
