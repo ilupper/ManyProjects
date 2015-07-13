0fa0a26b8bb7fee5c159a58d8a2030429b38567e
ilupper <lupma@yahoo.com>
2015-07-13 17:07:16 -0400

    Update NumberDeterminant.java

--- LupMavenWebapp/src/main/java/com/ilupper/util/NumberDeterminant.java
+++ LupMavenWebapp/src/main/java/com/ilupper/util/NumberDeterminant.java
@@ -1,7 +1,28 @@
 package com.ilupper.util;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class TypeDeterminant {
+    
+    /**
+     * Has to check for java.time.DateTimeParseException
+     * 
+     * @param dateInString
+     * @return
+     */
+    public static LocalDate convertToLocalDate(String dateInString) {
+    
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        
+        try {
+            return LocalDate.parse(dateInString, formatter);
+        } 
+        catch (DateTimeException e) {return null;}
+        
+    }
     
     public static boolean isInteger(String possibleNum) {
         try {
