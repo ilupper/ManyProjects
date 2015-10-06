a08095f5147ec40e008ab52bb260293bacb09ad6
ilupper <yeslup@lupwin8.com>
2015-10-06 16:08:36 -0400

    multiple trains, multiple stations

--- LupMavenWebapp/src/main/java/com/ilupper/restful/SimpleService.java
+++ LupMavenWebapp/src/main/java/com/ilupper/restful/SimpleService.java
@@ -1,36 +1,59 @@
 package com.ilupper.restful;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Queue;
 
 import javax.ws.rs.*;
 
-import com.ilupper.domain.Passenger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ilupper.domain.Station;
 import com.ilupper.domain.Train;
 import com.ilupper.domain.Transit;
 
 @Path("rs/simple")
-public class SimpleService
-{
- 
-    @GET
-    @Path("simpleGET")
-    public String getStringAndDate() {
-        return "a string. Date = " + LocalDate.now();
-    }
-    
-    @GET
-    @Path("transit")
-    public String getTrainOnboard() {
-    	HashSet<Train> trains = Transit.getTransit().getTrains();
-    	HashSet<Station> stations = Transit.getTransit().getStations();
-    	
-    	Queue<Passenger> onTrain = train.getTraveling();
-    	Queue<Passenger> onPlatform = station.getWaiting();
-    	return "Number onboard train is " + onTrain.size() + "\n" + 
-    			"Number waiting on platform is " + onPlatform.size() ;
-    }
-    
+public class SimpleService {
+	
+	Logger log;
+	public SimpleService() {
+		log = LoggerFactory.getLogger(this.getClass());
+	}
+
+	@GET
+	@Path("simpleGET")
+	public String getStringAndDate() {
+		return "a string. Date = " + LocalDate.now();
+	}
+
+	@GET
+	@Path("transit")
+	public String getTrainOnboard() {
+		Transit transit = Transit.getInstance();
+		HashSet<Train> trains = null;
+		HashMap<String, Station> stations = null;
+
+		transit.setInitialTransit(1, 1);
+
+		trains = Transit.getInstance().getTrains();
+		stations = Transit.getInstance().getStations();
+
+		Integer totalTrain = 0, totalStation = 0;
+		
+		for (Train train: trains) 
+			totalTrain += train.getTraveling().size();
+		Collection<Station> collStations = stations.values();
+		for (Station station: collStations) { 
+			totalStation += station.getWaiting().size();
+		}
+
+		log.warn("Number onboard trains is " + totalTrain + " in a set of " + trains.size() + " train(s) \n"
+				+ "Number waiting on platform is " + totalStation + " in a set of " + stations.size() + " station(s)");
+		return "Number onboard trains is " + totalTrain + " in a set of " + trains.size() + " train(s) \n"
+				+ "Number waiting on platform is " + totalStation + " in a set of " + stations.size() + " station(s)";
+
+	}
+
 }
